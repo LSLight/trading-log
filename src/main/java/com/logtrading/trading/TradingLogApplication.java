04f1cd7f9f005e529a81673e36aa358a7ae5fa08
Lee Subin <99sbin@gmail.com>
2026-01-20 18:37:00 +0900

    Initial commit

---
+++ src/main/java/com/logtrading/trading/TradingLogApplication.java
@@ -0,0 +1,13 @@
+package com.logtrading.trading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradingLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/logtrading/trading/TradingLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logtrading.trading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
